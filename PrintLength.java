7e2d71b57e341281aff80fa74417bf056e524a5c
vinocz <vinothkmr478@gmail.com>
2022-09-22 12:02:02 +0530

    length

--- PrintLength.java
+++ PrintLength.java
@@ -2,7 +2,7 @@ package day3;
 
 public class PrintLength {
 	public static void main(String[]args) {
-		String word="vinoth";
+		String word="ajith";
 		int length =word.length();
 		System.out.println(length);
 		
